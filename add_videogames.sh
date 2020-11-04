90a3c31cc48b461d749d03a1f600728d12b64ac1
tamani-coding <tamani.coding@gmail.com>
2020-11-04 18:59:19 +0100

    more examples, search api

--- add_videogames.sh
+++ add_videogames.sh
@@ -11,3 +11,11 @@ echo "{ \"title\": \"Edna & Harvey: The Breakout\", \"releaseDate\": \"2008-06-0
 echo "{ \"title\": \"God of War\", \"releaseDate\": \"2018-04-20\", \"publisher\": \"Sony Interactive Entertainment\" }" | curl -d @- -X POST http://localhost:8080/videogame/create
 
 echo "{ \"title\": \"The Elder Scrolls V: Skyrim\", \"releaseDate\": \"2011-11-11\", \"publisher\": \"Bethesda Softworks\" }" | curl -d @- -X POST http://localhost:8080/videogame/create
+
+echo "{ \"title\": \"The Secret of Monkey Island\", \"releaseDate\": \"1990-10-15\", \"publisher\": \"Lucasarts\" }" | curl -d @- -X POST http://localhost:8080/videogame/create
+
+echo "{ \"title\": \"Half-Life\", \"releaseDate\": \"1998-11-19\", \"publisher\": \"Valve\" }" | curl -d @- -X POST http://localhost:8080/videogame/create
+
+echo "{ \"title\": \"Warcraft III: Reforged\", \"releaseDate\": \"2002-07-03\", \"publisher\": \"Activision Blizzard\" }" | curl -d @- -X POST http://localhost:8080/videogame/create
+
+echo "{ \"title\": \"Street Fighter II\", \"releaseDate\": \"1991-02-01\", \"publisher\": \"Capcom\" }" | curl -d @- -X POST http://localhost:8080/videogame/create
